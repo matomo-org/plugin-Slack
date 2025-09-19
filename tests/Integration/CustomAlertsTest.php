7452f1aa43812b2e9e8d0845bc55c273da41f75e
Altamash Shaikh <altu9594@gmail.com>
2025-09-19 09:11:27 +0530

    Adds depenedent plugin to add tests for CustomAlerts plugin

---
+++ tests/Integration/CustomAlertsTest.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Matomo - free/libre analytics platform
+ *
+ * @link    https://matomo.org
+ * @license https://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ */
+
+namespace Piwik\Plugins\Slack\tests;
+
+use Piwik\Plugins\CustomAlerts\CustomAlerts;
+use Piwik\Tests\Framework\TestCase\IntegrationTestCase;
+
+/**
+ * @group Slack
+ * @group CustomAlertsTest
+ * @group Plugins
+ */
+
+class CustomAlertsTest extends IntegrationTestCase
+{
+    public function testGetReportMediumOptions()
+    {
+        $this->assertEquals([
+            ['key' => 'email', 'value' => 'CustomAlerts_MediumEmail', 'disabled' => false],
+            ['key' => 'mobile', 'value' => 'CustomAlerts_MediumMobile', 'disabled' => false],
+            ['key' => 'slack', 'value' => 'CustomAlerts_MediumSlack', 'disabled' => false],
+        ], CustomAlerts::getReportMediumOptions());
+    }
+}
